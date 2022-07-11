472f1288d6ae7f374e80f2a11fb16cdaa0affcd7
Cedric Wu Tchan Ki <cedric@wutchanki.com>
2022-07-10 23:42:47 -0400

    ajout de 2 sections a modifier plus tard - prochaine etape footer

--- src/assets/pages/Home.js
+++ src/assets/pages/Home.js
@@ -1,17 +1,26 @@
 import React from "react";
 import Navbar from "../../components/Navbar";
-import { HeroContainer, HeroSlogan, HeroTextContainer, HeroVideoBackground } from "../../styles/Hero.style";
+import { HeroContainer, HeroSlogan, HeroTextContainer, HeroVideoBackground, BodyContainer, TextDescriptionContainer, TextSizeLarge, CarrousselContainer } from "../../styles/Home.style";
 import Video from '../video/hero.mp4';
 const Home = () => {
     return(
       <>
       <Navbar/>
+      <BodyContainer>
       <HeroContainer>
         <HeroVideoBackground autoPlay loop muted src={Video} type='video/mp4'/>
         <HeroTextContainer>
           <HeroSlogan>Digital creativity and self taught productivity</HeroSlogan>
         </HeroTextContainer>
       </HeroContainer>
+      <CarrousselContainer>
+        <TextSizeLarge>This section will hold a caroussel</TextSizeLarge>
+      </CarrousselContainer>
+      <TextDescriptionContainer>
+        <TextSizeLarge>This section will hold a short description about me</TextSizeLarge>
+      </TextDescriptionContainer>
+
+      </BodyContainer>
       </>
     )
   };
--- src/styles/Home.style.js
+++ src/styles/Home.style.js
@@ -1,6 +1,34 @@
 import styled from 'styled-components';
 import video from '../assets/video/hero.mp4';
 
+export const BodyContainer = styled.div`
+    width: 100%;
+    height: 100%;
+    margin: auto;
+    background-color: black;
+`;
+
+export const TextDescriptionContainer = styled.div`
+    width: 100%;
+    height: 40vh;
+    background-color: red;
+`;
+
+export const CarrousselContainer = styled.div`
+    width: 100%;
+    height: 40vh;
+    background-color: blue;
+`;
+
+export const TextSizeLarge = styled.h2`
+    color: white;
+    font-size: 2em;
+    font-family: 'Courier New', Courier, monospace;
+    margin: 0;
+    padding: 10%;
+`;
+
+
 export const HeroContainer = styled.div`
     width: 100%;
     height: 100vh;
@@ -29,9 +57,8 @@ export const HeroTextContainer = styled.div`
 
 export const HeroSlogan = styled.h1`
     color: salmon;
-    font-size: 3em;
+    font-size: 4em;
     font-family: 'Courier New', Courier, monospace;
-
 `;
 
 
