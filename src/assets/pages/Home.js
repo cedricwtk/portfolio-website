4f2824b4070942c028ff147e8b5f717a3da210a2
Cedric Wu Tchan Ki <cedric@wutchanki.com>
2022-08-15 00:16:15 -0400

    Ajout de photo sans bg pour creer l'espace a propos de moi

--- src/assets/pages/Home.js
+++ src/assets/pages/Home.js
@@ -4,27 +4,27 @@ import Navbar from "../../components/Navbar";
 import { HeroContainer, HeroSlogan, HeroTextContainer, HeroVideoBackground, BodyContainer, TextDescriptionContainer, TextSizeLarge, CarrousselContainer } from "../../styles/Home.style";
 import Video from '../video/hero.mp4';
 const Home = () => {
-    return(
-      <>
-      <Navbar/>
+  return (
+    <>
+      <Navbar />
       <BodyContainer>
-      <HeroContainer>
-        <HeroVideoBackground autoPlay loop muted src={Video} type='video/mp4'/>
-        <HeroTextContainer>
-          <HeroSlogan>Digital creativity and self taught productivity</HeroSlogan>
-        </HeroTextContainer>
-      </HeroContainer>
-      <CarrousselContainer>
-        <TextSizeLarge>This section will hold a caroussel</TextSizeLarge>
-      </CarrousselContainer>
-      <TextDescriptionContainer>
-        <TextSizeLarge>This section will hold a short description about me</TextSizeLarge>
-      </TextDescriptionContainer>
+        <HeroContainer>
+          <HeroVideoBackground autoPlay loop muted src={Video} type='video/mp4' />
+          <HeroTextContainer>
+            <HeroSlogan>Digital creativity and self taught productivity</HeroSlogan>
+          </HeroTextContainer>
+        </HeroContainer>
+        <CarrousselContainer>
+          <TextSizeLarge>This section will hold a caroussel</TextSizeLarge>
+        </CarrousselContainer>
+        <TextDescriptionContainer>
+          <TextSizeLarge>This section will hold a short description about me</TextSizeLarge>
+        </TextDescriptionContainer>
 
       </BodyContainer>
-      <Footer/>
-      </>
-    )
-  };
-  
-  export default Home;
+      <Footer />
+    </>
+  )
+};
+
+export default Home;
